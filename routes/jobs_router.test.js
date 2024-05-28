88b382b12c211ccf83a51ea35f7bc6db1fb9d9c3
amaldev <amaldevm19@gmail.com>
2024-05-28 09:23:12 +0400

    modified jobs_router.test.js

--- routes/jobs_router.test.js
+++ routes/jobs_router.test.js
@@ -1,25 +1,10 @@
 const request = require("supertest");
 const app = require("../app");
 
-let cookie;
-
-beforeAll(async () => {
-    const response = await request(app)
-        .post("/api/users/login")
-        .send({
-            employeeID: "25002",
-            password: "123456"
-        }).set('Accept', 'application/json');
-
-    expect(response.statusCode).toBe(200);
-    console.log(response)
-    cookie = response.headers['set-cookie']; // Capture the session cookie
-});
-
 describe("Testing /jobs/ route", () => {
     it("It should respond with Assign Jobs To Employees Using CSV file", async () => {
         const agent = request.agent(app);
-        await agent.post('/api/users/login').send({ employeeID: '25002', password: '123456' }).set('Accept', 'application/json');
+        await agent.post('/api/users/login').send({ employeeID: '25002', password: '123456' });
         const response = await agent.get("/jobs/assign-jobs-manually");
 
         expect(response.statusCode).toBe(200);
