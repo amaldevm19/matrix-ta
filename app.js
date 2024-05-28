44cd1fc5e2a4b608aff49265f5f6be3d91ae270e
amaldev <amaldevm19@gmail.com>
2024-05-28 06:56:04 +0400

    modified app.js

--- app.js
+++ app.js
@@ -69,6 +69,7 @@ app.use(
     secret: 'your-secret-key',
     resave: false,
     saveUninitialized: false,
+    cookie: { secure: false } 
   })
 );
 app.use(express.static(path.join(__dirname, 'views'))) 
