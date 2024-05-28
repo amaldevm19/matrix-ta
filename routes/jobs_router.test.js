808ec748d08c365a9e70e4d76ee2f66adb57c6e2
amaldev <amaldevm19@gmail.com>
2024-05-28 09:15:59 +0400

    modified jobs_router.test.js

--- routes/jobs_router.test.js
+++ routes/jobs_router.test.js
@@ -17,8 +17,10 @@ beforeAll(async () => {
 });
 
 describe("Testing /jobs/ route", () => {
-    test("It should respond with Assign Jobs To Employees Using CSV file", async () => {
-        const response = await request(app).get("/jobs/assign-jobs-manually").set("Cookie", cookie);;
+    it("It should respond with Assign Jobs To Employees Using CSV file", async () => {
+        const agent = request.agent(app);
+        await agent.post('/api/users/login').send({ employeeID: '25002', password: '123456' }).set('Accept', 'application/json');
+        const response = await agent.get("/jobs/assign-jobs-manually");
 
         expect(response.statusCode).toBe(200);
         expect(response.text).toContain('<title>TNA PROXY SERVER | Assign Jobs To Employees Using CSV file</title>');
