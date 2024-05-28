b3d97ee99097771d3b9b8261b035fd996a4e81af
amaldev <amaldevm19@gmail.com>
2024-05-28 05:51:17 +0400

    modified home.test

--- routes/home_router.test.js
+++ routes/home_router.test.js
@@ -5,5 +5,6 @@ describe("Test the root path", () => {
         const response = await request(app).get("/");
         expect(response.statusCode).toBe(200);
         expect(response.text).toContain('<title>TNA PROXY SERVER | Home</title>');
+        expect(response.text).toContain('<a class="nav-link" href="/users/logout">Logout</a>');
     });
 });
