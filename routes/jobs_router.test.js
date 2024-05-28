b300aca47f4dca8870f815331e6cc26a3ba30f15
amaldev <amaldevm19@gmail.com>
2024-05-28 06:26:05 +0400

    added jobs_router.test.js

---
+++ routes/jobs_router.test.js
@@ -0,0 +1,28 @@
+const request = require("supertest");
+const app = require("../app");
+
+let cookie;
+
+beforeAll(async () => {
+    const response = await request(app)
+        .post("/users/login")
+        .send({
+            username: "25002",
+            password: "123456"
+        });
+
+    expect(response.statusCode).toBe(200);
+    cookie = response.headers['set-cookie']; // Capture the session cookie
+});
+
+describe("Testing /jobs/ route", () => {
+    test("It should respond with Assign Jobs To Employees Using CSV file", async () => {
+        const response = await request(app)
+            .get("/jobs/assign-jobs-manually")
+            .set("Cookie", cookie);
+
+        expect(response.statusCode).toBe(200);
+        expect(response.text).toContain('<title>TNA PROXY SERVER | Assign Jobs To Employees Using CSV file</title>');
+        expect(response.text).toContain('<h4 class="mx-auto">Assign Jobs To Employees Using CSV file</h4>');
+    });
+});
