84a82439ec988ce8bf5e92501352a28ff161bc09
amaldev <amaldevm19@gmail.com>
2024-05-30 06:48:01 +0400

    modified erp_transaction_page_router.test.js

--- routes/erp_transaction_page_router.test.js
+++ routes/erp_transaction_page_router.test.js
@@ -57,3 +57,43 @@ describe("Testing /api/erp-transaction/pending-data route", () => {
         
     });
 });
+
+
+
+describe("Testing /erp-transactions/status route", () => {
+    it("It should respond with ERP Timesheet Sync Completed Page", async () => {
+        const agent = request.agent(app);
+        await agent.post('/api/users/login').send({ employeeID: '25002', password: '123456' });
+        const response = await agent.get("/erp-transactions/status");
+
+        expect(response.statusCode).toBe(200);
+        expect(response.text).toContain('<title>TNA PROXY SERVER | ERP Timesheet Sync Completed</title>');
+        expect(response.text).toContain('<h4 class="mx-auto">ERP Timesheet Sync Completed</h4>');
+    });
+});
+
+describe("Testing /api/erp-transaction/status route", () => {
+    it("It should respond with ERP Timesheet Sync Completed JSON data", async () => {
+        const agent = request.agent(app);
+        await agent.post('/api/users/login').send({ employeeID: '25002', password: '123456' });
+        const response = await agent.get("/api/erp-transaction/status").query({
+            page:1,
+            size:10,
+            EmployeeId:"",
+            FromDate:"",
+            ToDate:"",
+            JobCode:"",
+            DepartmentId:"",
+            UserCategoryId:"",
+            EmployeeCategoryId:"",
+            DesignationId:"",
+            SectionId:""
+        });
+        expect(response.statusCode).toBe(200);
+        expect(response.body.status).toBe("ok");
+        expect(response.headers['content-type']).toMatch(/json/);
+        expect(Array.isArray(response.body.data)).toBe(true);
+        expect(response.body.data.length).toBeGreaterThan(0);
+        
+    });
+});v
