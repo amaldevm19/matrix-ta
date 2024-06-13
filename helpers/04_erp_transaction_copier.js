3a99f2b994f04e7c7a3d2f210cde28a644018ada
amaldev <amaldevm19@gmail.com>
2024-06-13 22:40:30 +0400

    Updated PxERPTransactionTableBuilder query

--- helpers/04_erp_transaction_copier.js
+++ helpers/04_erp_transaction_copier.js
@@ -83,8 +83,7 @@ async function PxERPTransactionTableBuilder({FromDate='', ToDate='',DepartmentId
                 (CAST(Target.TotalHours AS decimal(4, 1)) <> 
                     CAST( 
                         CASE  
-                            WHEN Source.TotalHours < CAST( 8.0 AS DECIMAL(4,1))THEN Source.TotalHours
-                            WHEN Source.TotalHours - COALESCE(Source.BreakHour, 1) - COALESCE(Source.TravelHour, 0) - COALESCE(Source.DeductionHours, 0) <= 0 THEN Source.TotalHours  
+                            WHEN Source.TotalHours <= CAST( 8 AS DECIMAL(4,1)) AND Source.TotalHours > 0 THEN 8   
                             WHEN Target.TotalHours > MaxJobHourPerDay 
                                 OR Source.TotalHours - COALESCE(BreakHour, 1) - COALESCE(TravelHour, 0)-COALESCE(DeductionHours, 0) > MaxJobHourPerDay THEN MaxJobHourPerDay
                             ELSE Source.TotalHours - COALESCE(BreakHour, 1) - COALESCE(TravelHour, 0) - COALESCE(DeductionHours, 0)
@@ -94,8 +93,7 @@ async function PxERPTransactionTableBuilder({FromDate='', ToDate='',DepartmentId
         UPDATE SET
             TotalHours = CAST(
                         CASE 
-                            WHEN Source.TotalHours < CAST( 8.0 AS DECIMAL(4,1))THEN Source.TotalHours
-                            WHEN Source.TotalHours - COALESCE(Source.BreakHour, 1) - COALESCE(Source.TravelHour, 0) - COALESCE(Source.DeductionHours, 0) <= 0 THEN Source.TotalHours
+                            WHEN Source.TotalHours <= CAST( 8 AS DECIMAL(4,1)) AND Source.TotalHours > 0 THEN 8 
                             WHEN Target.TotalHours > COALESCE(Source.MaxJobHourPerDay, Target.TotalHours) 
                                 OR Source.TotalHours-COALESCE(BreakHour, 1)-COALESCE(TravelHour, 0) - COALESCE(DeductionHours, 0) > COALESCE(Source.MaxJobHourPerDay, Source.TotalHours)  THEN MaxJobHourPerDay
                             ELSE Source.TotalHours - COALESCE(BreakHour, 1) - COALESCE(TravelHour, 0) - COALESCE(DeductionHours, 0)
@@ -124,8 +122,7 @@ async function PxERPTransactionTableBuilder({FromDate='', ToDate='',DepartmentId
                 Source.TransDate,
                 Source.projId,
                 CASE
-                    WHEN Source.TotalHours < CAST( 8.0 AS DECIMAL(4,1))THEN Source.TotalHours
-                    WHEN Source.TotalHours - COALESCE(Source.BreakHour, 1) - COALESCE(Source.TravelHour, 0) - COALESCE(Source.DeductionHours, 0) <= 0 THEN Source.TotalHours
+                    WHEN Source.TotalHours <= CAST( 8 AS DECIMAL(4,1)) AND Source.TotalHours > 0 THEN 8 
                     WHEN Source.TotalHours-COALESCE(BreakHour, 1)-COALESCE(TravelHour, 0) - COALESCE(DeductionHours, 0) > COALESCE(Source.MaxJobHourPerDay, Source.TotalHours) THEN COALESCE(Source.MaxJobHourPerDay, Source.TotalHours)
                     ELSE Source.TotalHours - COALESCE(BreakHour, 1) - COALESCE(TravelHour, 0)-COALESCE(DeductionHours, 0)
                 END,
@@ -157,135 +154,6 @@ async function PxERPTransactionTableBuilder({FromDate='', ToDate='',DepartmentId
         return {status:"not ok",data:"",error};
     }
 }
-/*
-async function getTimesheetFromERPTransactionMstTable({
-    sendingCount,
-    EmployeeId,
-    JobCode,
-    DepartmentId,
-    UserCategoryId,
-    EmployeeCategoryId,
-    DesignationId,
-    SectionId, 
-    FromDate, 
-    ToDate,
-    SyncCompleted,
-    }){
-    try {
-        await ProxyDbPool.connect();
-        const request = new sql.Request(ProxyDbPool);
-        try {
-            let result = await request.query(`
-                SELECT TOP (${sendingCount})[Id]
-                ,[HcmWorker_PersonnelNumber]
-                ,CONVERT(NVARCHAR(10), TransDate, 120) AS TransDate
-                ,[projId]
-                ,[TotalHours]
-                ,[CategoryId]
-                FROM [TNA_PROXY].[dbo].[Px_ERPTransactionMst]
-                WHERE 
-                ('${EmployeeId}' IS NULL OR '${EmployeeId}'='' OR HcmWorker_PersonnelNumber = '${EmployeeId}') AND
-                ('${JobCode}' IS NULL OR '${JobCode}'='' OR projId ='${JobCode}') AND
-                ('${DepartmentId}' IS NULL OR '${DepartmentId}'='' OR DepartmentId = ${DepartmentId?DepartmentId:0}) AND
-                ('${UserCategoryId}' IS NULL OR '${UserCategoryId}'='' OR UserCategoryId = ${UserCategoryId?UserCategoryId:0}) AND
-                ('${EmployeeCategoryId}' IS NULL OR '${EmployeeCategoryId}'='' OR EmployeeCategoryId = ${EmployeeCategoryId?EmployeeCategoryId:0}) AND
-                ('${DesignationId}' IS NULL OR '${DesignationId}'='' OR DesignationId = ${DesignationId?DesignationId:0}) AND
-                ('${SectionId}' IS NULL OR '${SectionId}'='' OR SectionId = ${SectionId?SectionId:0}) AND
-                (('${FromDate}'='' AND '${ToDate}'='') OR TransDate BETWEEN '${FromDate}' AND '${ToDate}') AND
-                (SyncCompleted = ${SyncCompleted} AND Error=0 );
-            `);
-            let message = `Started getting data from [TNA_PROXY].[dbo].[Px_ERPTransactionMst] for EmployeeId: ${EmployeeId}, JobCode: ${JobCode},DepartmentId: ${DepartmentId},UserCategoryId: ${UserCategoryId},EmployeeCategoryId:${EmployeeCategoryId},DesignationId: ${DesignationId},SectionId: ${SectionId},FromDate: ${FromDate},ToDate: ${ToDate}, and SyncCompleted: ${SyncCompleted}`;
-            console.log(message)
-            await MiddlewareHistoryLogger({EventType:EventType.INFORMATION,EventCategory:EventCategory.SYSTEM,EventStatus:EventStatus.STARTED,EventText:String(message)})
-            if(result?.recordset){
-                let message = `Successfully completed getting data from [TNA_PROXY].[dbo].[Px_ERPTransactionMst] for EmployeeId: ${EmployeeId}, JobCode: ${JobCode},DepartmentId: ${DepartmentId},UserCategoryId: ${UserCategoryId},EmployeeCategoryId:${EmployeeCategoryId},DesignationId: ${DesignationId},SectionId: ${SectionId},FromDate: ${FromDate},ToDate: ${ToDate}, and SyncCompleted: ${SyncCompleted}`;
-                console.log(message)
-                await MiddlewareHistoryLogger({EventType:EventType.INFORMATION,EventCategory:EventCategory.SYSTEM,EventStatus:EventStatus.STARTED,EventText:String(message)});
-                return {data:result.recordset, error:"",status:"ok", request}
-            }
-            
-            throw new Error("Error in getTimesheetFromERPTransactionMstTable function returned null rows")
-
-        } catch (error) {
-            let message =`Error in getTimesheetFromERPTransactionMstTable function : ${error}`;
-            console.log(message)
-            await MiddlewareHistoryLogger({EventType:EventType.ERROR,EventCategory:EventCategory.SYSTEM,EventStatus:EventStatus.FAILED,EventText:String(message)})
-            return {data:"", error:message,status:"not ok"}
-        }
-    } catch (error) {
-        let message =`Error connecting to the database in getTimesheetFromERPTransactionMstTable function : ${error}`;
-        console.log(message)
-        await MiddlewareHistoryLogger({EventType:EventType.ERROR,EventCategory:EventCategory.SYSTEM,EventStatus:EventStatus.FAILED,EventText:String(message)})
-        return {data:"", error:message,status:"not ok"}
-    }
-}
-
-
-async function updateERPTransactionStatus(pendingD365ResponseArray) {
-    try {
-        await ProxyDbPool.connect();
-        const request = new sql.Request(ProxyDbPool);
-        try {
-            let results =[];
-            let message = `Starting updating [TNA_PROXY].[dbo].[Px_ERPTransactionMst] with D365_response in updateERPTransactionStatus function`;
-            console.log(message)
-            await MiddlewareHistoryLogger({EventType:EventType.INFORMATION,EventCategory:EventCategory.SYSTEM,EventStatus:EventStatus.STARTED,EventText:String(message)});
-            for (let index = pendingD365ResponseArray.length; index > 0; ) {
-                const element = pendingD365ResponseArray[index-1]
-                let query = "";
-                if (element.Error) {
-                    query = `UPDATE [TNA_PROXY].[dbo].[Px_ERPTransactionMst] 
-                            SET Error = 1, ErrorText = '${element.ErrorTxt}' 
-                            WHERE HcmWorker_PersonnelNumber = '${element.HcmWorker_PersonnelNumber}'
-                            AND TransDate = '${element.TransDate.slice(0, 10)} 00:00:00.000'
-                            AND projId = '${element.ProjId}'`;
-                } else {
-                    query = `UPDATE [TNA_PROXY].[dbo].[Px_ERPTransactionMst] 
-                            SET SyncCompleted = 1 
-                            WHERE HcmWorker_PersonnelNumber = '${element.HcmWorker_PersonnelNumber}'
-                            AND TransDate = '${element.TransDate.slice(0, 10)} 00:00:00.000'
-                            AND projId = '${element.ProjId}'`;
-                }
-                try {
-                    const result = await request.query(query);
-                    if (result?.rowsAffected[0]) {
-                        console.log({ ...element, SyncCompleted: 1 })
-                        results.push({ ...element, SyncCompleted: 1 });
-                        pendingD365ResponseArray.pop();
-                        index--;
-                    } else {
-                        results.push(element);
-                    }
-                } catch (error) {
-                    let message = `Error in updating status for HcmWorker_PersonnelNumber: ${element.HcmWorker_PersonnelNumber} and Message: ${error.message}`;
-                    console.log(message);
-                    await MiddlewareHistoryLogger({EventType:EventType.INFORMATION,EventCategory:EventCategory.SYSTEM,EventStatus:EventStatus.COMPLETED,EventText:String(message)});
-                    setTimeout(()=>{},2000)
-                }
-            }
-            if(results){
-                let message = `Completed updating [TNA_PROXY].[dbo].[Px_ERPTransactionMst] with D365_response in updateERPTransactionStatus function`;
-                console.log(message)
-                await MiddlewareHistoryLogger({EventType:EventType.INFORMATION,EventCategory:EventCategory.SYSTEM,EventStatus:EventStatus.COMPLETED,EventText:String(message)});
-            }
-            return {data:results, error:"", status:"ok"}
-        } catch (error) {
-            let message =`Error in updateERPTransactionStatus function : ${error}`;
-            console.log(message)
-            await MiddlewareHistoryLogger({EventType:EventType.ERROR,EventCategory:EventCategory.SYSTEM,EventStatus:EventStatus.FAILED,EventText:String(message)})
-            return {data:"", error, status:"not ok"}
-        }
-    } catch (error) {
-        let message =`Error connecting to the database in updateERPTransactionStatus function : ${error}`;
-        console.log(message)
-        await MiddlewareHistoryLogger({EventType:EventType.ERROR,EventCategory:EventCategory.SYSTEM,EventStatus:EventStatus.FAILED,EventText:String(message)})
-        return {data:"", error, status:"not ok"}
-    }
-
-}
-
-
-*/
 
 async function getTimesheetFromERPTransactionMstTable({
     EmployeeId,
@@ -349,8 +217,6 @@ async function getTimesheetFromERPTransactionMstTable({
         throw error;
     }
 }
-
-
 
 async function updateERPTransactionStatus(postingResult) {
     try {
@@ -416,7 +282,6 @@ async function updateERPTransactionStatus(postingResult) {
     }
 }
 
- // Define a function to sanitize the input
  function sanitizeInput(input) {
     // Use a regular expression to remove characters other than a-zA-Z0-9 and hyphen (-)
     return input.trim().replace(/[^a-zA-Z0-9-]/g, '');
